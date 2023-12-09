95c0f8eaf28dea6d26bac11873dd3b63900daaf0
L1ghtee <nikorak20@gmail.com>
2023-12-09 16:22:46 +0200

    51:39

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
 import express from "express";
 import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
 import mongoose from "mongoose";
+import { validationResult } from "express-validator";
 
-mongoose.connect('mongodb+srv://MrL1ght:misha0278@cluster0.rulbh.mongodb.net/?retryWrites=true&w=majority')
-.then(()=>console.log('DB ok'))
-.catch((err)=>console.log("DB error", err));
+import { registerValidation } from "./validations/auth.js";
+
+import UserModel from "./models/User.js";
+
+mongoose
+  .connect(
+    "mongodb+srv://MrL1ght:misha0278@cluster0.rulbh.mongodb.net/blog?retryWrites=true&w=majority"
+  )
+  .then(() => console.log("DB ok"))
+  .catch((err) => console.log("DB error", err));
 
 const app = express();
 
 app.use(express.json());
 
+app.post("/auth/register", registerValidation, async (req, res) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json(errors.array());
+  }
 
-app.post("/auth/register", (req, res) => {
-    
+  const password = req.body.password;
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+  const doc = new UserModel({
+    email: req.body.email,
+    fullName: req.body.fullName,
+    avatarUrl: req.body.avatarUrl,
+    passwordHash,
+  });
+  const user = await doc.save();
+  res.json(user);
 });
 
 app.listen(4444, (err) => {
--- validations/auth.js
+++ validations/auth.js
@@ -1,5 +1,8 @@
 import {body} from "express-validator"
 
-const registerValidation = [
-    body('email').isEmail()
+export const registerValidation = [
+    body('email', 'Incorrect Email ').isEmail(),
+    body('password', 'Lenghth of the passwort should be more than 5 symbols').isLength({min: 5}),
+    body('fullName', 'Please enter corect name').isLength({min: 3}),
+    body('avatarUrl', 'Incorrect Url for picture').optional().isURL(),
 ];
