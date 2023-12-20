3cb57327bae46290382154ffe309ae0e082264ec
L1ghtee <nikorak20@gmail.com>
2023-12-20 15:26:48 +0200

    fix bags

--- controllers/PostsController.js
+++ controllers/PostsController.js
@@ -35,7 +35,7 @@ export const getOne = async (req, res) => {
       { _id: postId },
       { $inc: { viewsCount: 1 } },
       { returnDocument: "after" }
-    );
+    ).populate("user");
 
     if (!updatedPost) {
       return res.status(404).json({
